e2eb3e475bfdffd27c814e60a69b596d8309def1
Endy José Alexandre Melo dos Santos <endy.santos@grupofasitec.com.br>
2023-10-11 09:34:18 -0300

    changes!: establishing files structure

--- mock_store/lib/main.dart
+++ mock_store/lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(MockStore());
-}
+void main() => runApp(MockStore());
 
 class MockStore extends StatelessWidget {
   const MockStore({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('MockStore')),
-      ),
+    return GetMaterialApp(
+      title: 'Mock Store',
     );
   }
 }
