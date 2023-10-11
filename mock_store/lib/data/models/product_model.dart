56eaf1b63edd9f6f0467789ee880c467a845fc36
Endy José Alexandre Melo dos Santos <endy.santos@grupofasitec.com.br>
2023-10-11 10:07:26 -0300

    changes: defining product model

---
+++ mock_store/lib/data/models/product_model.dart
@@ -0,0 +1,63 @@
+import '../../domain/entities/product.dart';
+
+class ProductModel extends Product {
+  ProductModel({
+    required int id,
+    required String title,
+    required double price,
+    required String description,
+    required String image,
+    required String category,
+    required Rating rating,
+  }) : super(
+    id: id,
+    title: title,
+    price: price,
+    description: description,
+    image: image,
+    category: category,
+    rating: rating,
+  );
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      title: json['title'],
+      price: json['price'],
+      description: json['description'],
+      image: json['image'],
+      category: json['category'],
+      rating: Rating(
+        rate: json['rating']['rate'],
+        count: json['rating']['count'],
+      ),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'price': price,
+      'description': description,
+      'image': image,
+      'category': category,
+      'rating': {
+        'rate': rating.rate,
+        'count': rating.count,
+      },
+    };
+  }
+
+  Product toEntity() {
+    return Product(
+      id: id,
+      title: title,
+      price: price,
+      description: description,
+      image: image,
+      category: category,
+      rating: rating,
+    );
+  }
+}
