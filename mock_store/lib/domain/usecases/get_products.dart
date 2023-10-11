c338feebffc79e3d18c4fc0125d351f6a14d6314
Endy José Alexandre Melo dos Santos <endy.santos@grupofasitec.com.br>
2023-10-11 10:27:02 -0300

    changes: defining getProducts usecase

---
+++ mock_store/lib/domain/usecases/get_products.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:mock_store/core/errors/failures.dart';
+import 'package:mock_store/data/repositories/product_repository.dart';
+import 'package:mock_store/domain/entities/product.dart';
+
+class GetProducts {
+  final ProductRepository repository;
+
+  GetProducts(this.repository);
+
+  Future<Either<Failure, List<Product>>> call() {
+    return repository.getProducts();
+  }
+}
